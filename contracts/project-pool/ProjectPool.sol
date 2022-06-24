16ff5208741983859695946e6dc1c7a01a13a17c
LidoKing <kinglido203@gmail.com>
2022-06-24 15:39:42 +0800

    feat: added transaction fees for nft buys

--- contracts/project-pool/ProjectPool.sol
+++ contracts/project-pool/ProjectPool.sol
@@ -11,15 +11,19 @@ contract ProjectPool is IERC721Receiver {
     uint256 public constant LOCK_MINT_AMOUNT = 500 * (10**18);
 
     IPoolToken poolToken;
-    IERC20 FUR;
+    //IERC20 FUR;
     IERC721 NFT;
 
     address public factory;
     address public owner;
 
+    // 0 - 100
+    uint128 swapFeeRate = 1;
+    uint128 lockFeeRate = 3;
+
     struct LockInfo {
         address locker;
-        uint128 releaseTime;
+        uint96 releaseTime;
     }
     mapping(bytes32 => LockInfo) lockInfo;
 
@@ -45,8 +49,7 @@ contract ProjectPool is IERC721Receiver {
         address _owner
     ) external onlyFactory {
         poolToken = IPoolToken(_tokenAddress);
-        // FUR token address, current placeholder: USDC address
-        FUR = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+        //FUR = IERC20();
         NFT = IERC721(_nftAddress);
         owner = _owner;
     }
@@ -74,33 +77,40 @@ contract ProjectPool is IERC721Receiver {
     }
 
     modifier checkBalance(uint256 _amount) {
+        uint256 total;
+        if (_amount == SWAP_MINT_AMOUNT) {
+            total = _amount * (1 + swapFeeRate / 100);
+        } else if (_amount == LOCK_MINT_AMOUNT) {
+            total = _amount * (1 + lockFeeRate / 100);
+        }
+
         require(
-            poolToken.balanceOf(msg.sender) >= _amount,
+            poolToken.balanceOf(msg.sender) >= total,
             "ProjectPool: You don not have enough tokens."
         );
         require(
-            poolToken.allowance(msg.sender, address(this)) >= _amount,
+            poolToken.allowance(msg.sender, address(this)) >= total,
             "ProjectPool: Not enough amount of tokens approved."
         );
         _;
     }
 
     modifier unlockable(uint256 _id) {
-        bytes32 fId = furionId(_id);
+        bytes32 fId = getFurionId(_id);
 
         require(
             lockInfo[fId].locker == msg.sender,
             "ProjectPool: You did not lock this NFT."
         );
         require(
-            lockInfo[fId].releaseTime > uint128(block.timestamp),
+            lockInfo[fId].releaseTime > uint96(block.timestamp),
             "ProjectPool: NFT has already been released to public pool."
         );
         _;
     }
 
     modifier releasable(uint256 _id) {
-        bytes32 fId = furionId(_id);
+        bytes32 fId = getFurionId(_id);
 
         require(
             lockInfo[fId].locker != address(0),
@@ -111,14 +121,24 @@ contract ProjectPool is IERC721Receiver {
             "ProjectPool: NFT is locked forever."
         );
         require(
-            lockInfo[fId].releaseTime < uint128(block.timestamp),
+            lockInfo[fId].releaseTime < uint96(block.timestamp),
             "ProjectPool: Release time not yet reached."
         );
         _;
     }
 
-    function furionId(uint256 _id) public view returns (bytes32) {
+    function getFurionId(uint256 _id) public view returns (bytes32) {
         return keccak256(abi.encodePacked(address(NFT), _id));
+    }
+
+    function setBuyFeeRate(uint128 _rate) external onlyOwner {
+        require(_rate >= 0 && _rate <= 100, "ProjectPool: Invalid fee rate.");
+        swapFeeRate = _rate;
+    }
+
+    function setRedeemFeeRate(uint128 _rate) external onlyOwner {
+        require(_rate >= 0 && _rate <= 100, "ProjectPool: Invalid fee rate.");
+        lockFeeRate = _rate;
     }
 
     function changeOwner(address _newOwner) external onlyFactory {
@@ -132,21 +152,21 @@ contract ProjectPool is IERC721Receiver {
         NFT.safeTransferFrom(msg.sender, address(this), _id);
         poolToken.mint(msg.sender, SWAP_MINT_AMOUNT);
 
-        emit SoldNFT(furionId(_id), msg.sender);
+        emit SoldNFT(getFurionId(_id), msg.sender);
     }
 
     function buy(uint256 _id) external checkBalance(SWAP_MINT_AMOUNT) {
+        uint256 fee = (SWAP_MINT_AMOUNT * swapFeeRate) / 100;
         poolToken.burn(msg.sender, SWAP_MINT_AMOUNT);
-
-        // FUR.transferFrom(msg.sender, owner, fee);
+        poolToken.transferFrom(msg.sender, owner, fee);
 
         NFT.safeTransferFrom(address(this), msg.sender, _id);
 
-        emit BoughtNFT(furionId(_id), msg.sender);
+        emit BoughtNFT(getFurionId(_id), msg.sender);
     }
 
     /**
-     * @param _lockPeriod Amount of time locked in days
+     * @param _lockPeriod Amount of time locked in days, 0 is forever
      */
     function lock(uint256 _id, uint256 _lockPeriod) external toPool(_id) {
         require(
@@ -154,12 +174,12 @@ contract ProjectPool is IERC721Receiver {
             "ProjectPool: Lock time must be at least 30 days."
         );
 
-        bytes32 fId = furionId(_id);
+        bytes32 fId = getFurionId(_id);
 
         NFT.safeTransferFrom(msg.sender, address(this), _id);
 
         lockInfo[fId].locker = msg.sender;
-        lockInfo[fId].releaseTime = uint128(
+        lockInfo[fId].releaseTime = uint96(
             block.timestamp + _lockPeriod * 24 * 60 * 60
         );
 
@@ -173,11 +193,12 @@ contract ProjectPool is IERC721Receiver {
         unlockable(_id)
         checkBalance(LOCK_MINT_AMOUNT)
     {
-        bytes32 fId = furionId(_id);
+        bytes32 fId = getFurionId(_id);
 
         poolToken.burn(msg.sender, LOCK_MINT_AMOUNT);
 
-        // FUR.transferFrom(msg.sender, owner, fee);
+        // Undecided fee collection method for NFT redemption
+        // poolToken.transferFrom(msg.sender, owner, fee);
 
         delete lockInfo[fId];
 
@@ -187,7 +208,7 @@ contract ProjectPool is IERC721Receiver {
     }
 
     function release(uint256 _id) external onlyOwner {
-        bytes32 fId = furionId(_id);
+        bytes32 fId = getFurionId(_id);
 
         address sendRemainingTo = lockInfo[fId].locker;
 

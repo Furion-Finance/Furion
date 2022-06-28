7a54d39953132c9f2bfc01e88d92ac845661705f
LidoKing <kinglido203@gmail.com>
2022-06-28 18:09:42 +0800

    feat: added types for project pools testing

--- test/types.ts
+++ test/types.ts
@@ -1,8 +1,15 @@
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import type { Fixture } from "ethereum-waffle";
 
+import type { ProjectPool } from "../src/types/contracts/project-pool/ProjectPool";
+import type { ProjectPoolFactory } from "../src/types/contracts/project-pool/ProjectPoolFactory";
+import type { NFTest } from "../src/types/contracts/test-only/NFTest";
+
 declare module "mocha" {
   export interface Context {
+    nft: NFTest;
+    ppf: ProjectPoolFactory;
+    pool: ProjectPool;
     loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
     signers: Signers;
   }
@@ -10,4 +17,6 @@ declare module "mocha" {
 
 export interface Signers {
   admin: SignerWithAddress;
+  bob: SignerWithAddress;
+  alice: SignerWithAddress;
 }

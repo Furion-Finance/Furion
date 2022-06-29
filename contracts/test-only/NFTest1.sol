bc5d01b973c40f9f78c37a483fbf87e04063d38b
LidoKing <kinglido203@gmail.com>
2022-06-29 15:02:05 +0800

    chore: created new NFT collection for testing

---
+++ contracts/test-only/NFTest1.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFTest1 is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor(address[] memory _receivers) ERC721("NFTest1", "NFT1") {
+        uint256 length = _receivers.length;
+        for (uint256 i = 0; i < length; ) {
+            safeMint(_receivers[i]);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+}
--- test/types.ts
+++ test/types.ts
@@ -4,12 +4,15 @@ import type { Fixture } from "ethereum-waffle";
 import type { ProjectPool } from "../src/types/contracts/project-pool/ProjectPool";
 import type { ProjectPoolFactory } from "../src/types/contracts/project-pool/ProjectPoolFactory";
 import type { NFTest } from "../src/types/contracts/test-only/NFTest";
+import type { NFTest1 } from "../src/types/contracts/test-only/NFTest1";
 
 declare module "mocha" {
   export interface Context {
     nft: NFTest;
+    nft1: NFTest1;
     ppf: ProjectPoolFactory;
     pool: ProjectPool;
+    pool1: ProjectPool;
     loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
     signers: Signers;
   }

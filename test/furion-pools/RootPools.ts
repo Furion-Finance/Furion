334b26030f8f7db3a484479d570074066a9b7eda
LidoKing <kinglido203@gmail.com>
2022-07-07 12:23:01 +0800

    test(root pool): stake registered tokens to get FFT tests passed

--- test/furion-pools/RootPools.ts
+++ test/furion-pools/RootPools.ts
@@ -33,7 +33,7 @@ describe("Root Pools", function () {
     this.signers.alice = signers[2];
   });
 
-  describe("RootPoolFactory", function () {
+  xdescribe("RootPoolFactory", function () {
     // Deploy clean ProjectPoolFactory and NFTest contract
     beforeEach(async function () {
       // Deploy NFTs
@@ -131,6 +131,99 @@ describe("Root Pools", function () {
         );
       });
       */
+    });
+  });
+
+  describe("RootPool", async function () {
+    beforeEach(async function () {
+      // Deploy NFTs
+      const nftArtifact: Artifact = await artifacts.readArtifact("NFTest");
+      this.nft = <NFTest>(
+        await waffle.deployContract(this.signers.admin, nftArtifact, [
+          [
+            this.signers.admin.address,
+            this.signers.admin.address,
+            this.signers.admin.address,
+            this.signers.bob.address,
+            this.signers.bob.address,
+            this.signers.alice.address,
+          ],
+        ])
+      );
+
+      const nft1Artifact: Artifact = await artifacts.readArtifact("NFTest1");
+      this.nft1 = <NFTest1>(
+        await waffle.deployContract(this.signers.admin, nft1Artifact, [
+          [this.signers.admin.address, this.signers.bob.address, this.signers.alice.address],
+        ])
+      );
+
+      // Deploy project pool factory
+      const ppfArtifact: Artifact = await artifacts.readArtifact("ProjectPoolFactory");
+      this.ppf = <ProjectPoolFactory>await waffle.deployContract(this.signers.admin, ppfArtifact, []);
+
+      // Deploy root pool factory
+      const rpfArtifact: Artifact = await artifacts.readArtifact("RootPoolFactory");
+      this.rpf = <RootPoolFactory>await waffle.deployContract(this.signers.admin, rpfArtifact, []);
+
+      // Create project pools
+      const projectPool = await this.ppf.callStatic.createPool(this.nft.address);
+      await this.ppf.createPool(this.nft.address);
+      const pool1Address = await this.ppf.callStatic.createPool(this.nft1.address);
+      await this.ppf.createPool(this.nft1.address);
+      this.pp = <ProjectPool>await ethers.getContractAt("ProjectPool", projectPool);
+      this.pp1 = <ProjectPool>await ethers.getContractAt("ProjectPool", pool1Address);
+
+      // Bob sells NFT to project pools to get tokens (2000 F-NFT)
+      await this.nft.connect(this.signers.bob).approve(this.pp.address, 3);
+      await this.nft.connect(this.signers.bob).approve(this.pp.address, 4);
+      await this.pp.connect(this.signers.bob)["sell(uint256[])"]([3, 4]);
+      await this.nft1.connect(this.signers.bob).approve(this.pp1.address, 1);
+      await this.pp1.connect(this.signers.bob)["sell(uint256)"](1);
+
+      // Create root pool for F-NFT, token is FFT1
+      const rootPool = await this.rpf.callStatic.createPool([this.pp.address]);
+      await this.rpf.createPool([this.pp.address]);
+      this.rp = <RootPool>await ethers.getContractAt("RootPool", rootPool);
+    });
+
+    context("Staking", async function () {
+      it("should stake F-* tokens to get FFT with 0.01 ETH being the ref price of FFT for the first stake", async function () {
+        // Bob stakes all F-NFT
+        await this.pp.connect(this.signers.bob).approve(this.rp.address, su("2000"));
+        await this.rp.connect(this.signers.bob).stake(this.pp.address, su("2000"), 0);
+        // F-NFT in root pool
+        expect(await this.pp.balanceOf(this.rp.address)).to.equal(su("2000"));
+        // Bob gets ((15 / 1000) * 2000) / 0.01 * 0.99 (1% fee) = 2970 FFT1, based on the assumption that
+        // the NFT now is worth 15 ETH and FFT has a ref price of 0.01 ETH for the first stake
+        expect(await this.rp.balanceOf(this.signers.bob.address)).to.equal(su("2970"));
+        // Admin is fee receiver
+        expect(await this.rp.balanceOf(this.signers.admin.address)).to.equal(su("30"));
+      });
+
+      it("should stake F-* tokens to get FFT with ref price of FFT correctly calculated", async function () {
+        // Bob stakes 1000 F-NFT first
+        await this.pp.connect(this.signers.bob).approve(this.rp.address, su("2000"));
+        await this.rp.connect(this.signers.bob).stake(this.pp.address, su("1000"), 0);
+        // Bob gets ((15 / 1000) * 1000) / 0.01 * 0.99 (1% fee) = 1485 FFT1, based on the assumption that
+        // the NFT now is worth 15 ETH and FFT has a ref price of 0.01 ETH for the first stake
+        expect(await this.rp.balanceOf(this.signers.bob.address)).to.equal(su("1485"));
+        // Bob stakes remaining 1000 F-NFT, when price of NFT increases to 18 ETH
+        await this.rp.connect(this.signers.bob).stake(this.pp.address, su("1000"), 1);
+        // FFT ref price = sum / circulating supply = ((18 / 1000) * 1000) / 1500 = 0.012 ETH
+        // Bob gets ((18 / 1000) * 1000) / 0.012 * 0.99 (1% fee) = 1485 FFT1
+        expect(await this.rp.balanceOf(this.signers.bob.address)).to.equal(su("2970"));
+        expect(await this.rp.balanceOf(this.signers.admin.address)).to.equal(su("30"));
+        expect(await this.pp.balanceOf(this.rp.address)).to.equal(su("2000"));
+      });
+
+      it("should not allow staking of non-registered tokens", async function () {
+        // Bob tries to stake F-NFT1 which is not registered to the root pool
+        await this.pp1.connect(this.signers.bob).approve(this.rp.address, su("1000"));
+        await expect(this.rp.connect(this.signers.bob).stake(this.pp1.address, su("1000"), 0)).to.be.revertedWith(
+          "RootPool: Token not accepted in this pool.",
+        );
+      });
     });
   });
 });

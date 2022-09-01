b3381a982ce4927a8e3f434f5bb082ccc771c008
LidoKing <55234791+LidoKing@users.noreply.github.com>
2022-09-01 18:49:21 +0800

    Revert "Merge branch 'main' into lidoking/project-pool"
    This reverts commit b028507a5fee2393358d8099af9f0084607d5f83, reversing
    changes made to 358adae50d025823102914f77a10a7c736370d46.

--- contracts/utils/interfaces/IERC20Decimals.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-// OpenZeppelin Contracts v4.4.0 (token/ERC20/IERC20.sol)
-
-pragma solidity ^0.8.10;
-
-interface IERC20Decimals {
-    function decimals() external view returns (uint8);
-}

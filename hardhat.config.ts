507e8ff0f18650d1abbf165b8e1a11bc353f4c6a
LidoKing <55234791+LidoKing@users.noreply.github.com>
2022-09-14 11:26:43 +0800

    chore: imported tasks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
+import "@openzeppelin/hardhat-upgrades";
 import "@typechain/hardhat";
 import { config as dotenvConfig } from "dotenv";
 import "hardhat-deploy";
@@ -12,6 +13,7 @@ import { NetworkUserConfig } from "hardhat/types";
 import { resolve } from "path";
 import "solidity-coverage";
 
+import "./tasks";
 // tasks from FarmingPool
 import "./tasks/furion-farming/farmingPool";
 // tasks from FurionSwap
@@ -71,7 +73,7 @@ function getChainConfig(chain: keyof typeof chainIds): NetworkUserConfig {
   //     path: "m/44'/60'/0'/0",
   //   }
   // } else {
-  accounts = [`0x${privateKey}`];
+  accounts = [`${privateKey}`];
   // }
   return {
     accounts: accounts,
--- tasks/index.ts
+++ tasks/index.ts
@@ -1,15 +1,21 @@
 const deployChecker = require("./deploy/checker.ts");
 const deploySeparatePoolFactory = require("./deploy/furion-pools/separatePoolFactory");
+const deployFurionPricingOracle = require("./deploy/furion-pools/furionPricingOracle");
+const dpeloyAggregatePoolFactory = require("./deploy/furion-pools/aggregatePoolFactory");
 const deployFErc20 = require("./deploy/money-market/ferc20");
 const deployFEther = require("./deploy/money-market/fether");
 const deployNormalInterestRateModel = require("./deploy/money-market/normalInterestRateModel");
 const deployJumpInterestRateModel = require("./deploy/money-market/jumpInterestRateModel");
 const deployPriceOracle = require("./deploy/money-market/priceOracle");
 const deployRiskManager = require("./deploy/money-market/riskManager");
+const deployTestMarket = require("./deploy/money-market/testMarket");
 
 const upgradeFEther = require("./upgrade/money-market/fether");
 const upgradeFErc20 = require("./upgrade/money-market/ferc20");
 const upgradeRiskManager = require("./upgrade/money-market/riskManager");
+
+const createSeparatePool = require("./furion-pools/separatePoolFactory");
+const createAggregatePool = require("./furion-pools/aggregatePoolFactory");
 
 const accounts = require("./accounts");
 
@@ -17,13 +23,18 @@ export {
   accounts,
   deployChecker,
   deploySeparatePoolFactory,
+  deployFurionPricingOracle,
+  dpeloyAggregatePoolFactory,
   deployFErc20,
   deployFEther,
   deployNormalInterestRateModel,
   deployJumpInterestRateModel,
   deployPriceOracle,
   deployRiskManager,
+  deployTestMarket,
   upgradeFEther,
   upgradeFErc20,
   upgradeRiskManager,
+  createSeparatePool,
+  createAggregatePool,
 };

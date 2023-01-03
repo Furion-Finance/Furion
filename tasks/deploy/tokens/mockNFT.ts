9dc3e020c7a01725fb052998276dd205e9eada18
LidoKing <55234791+LidoKing@users.noreply.github.com>
2023-01-03 14:01:33 +0800

    feat: testnet nft deployment and minter registration tasks

--- tasks/deploy/tokens/mockNFT.ts
+++ tasks/deploy/tokens/mockNFT.ts
@@ -4,18 +4,65 @@ import type { TaskArguments } from "hardhat/types";
 import { readAddressList } from "../../../scripts/contractAddress";
 import { deploy, getNetwork, writeDeployment } from "../../helpers";
 
-task("deploy:MockCoolCats", "Deploy mock CoolCats").setAction(async function (
+let args = [
+  ["BAYC", "BAYC", "", 0, 10000],
+  ["MAYC", "MAYC", "", 0, 19431],
+  ["Otherdeed", "OTHR", "", 0, 100000],
+  ["BAKC", "BAKC", "", 0, 9602],
+  ["Azuki", "AZUKI", "", 0, 10000],
+  ["Doodles", "DOODLE", "", 0, 10000],
+  ["Meebits", "Meebits", "", 1, 20000],
+  ["Weirdo Ghost Gang", "GHOST", "", 0, 5556],
+  ["Catddle", "CAT", "", 1, 2048],
+  ["Mimic Shhans", "SHHANS", "", 1, 10020],
+];
+
+task("deploy:MockNFTs", "Deploy 11 mock NFT collections").setAction(async function (
+  taskArguments: TaskArguments,
+  { ethers },
+) {
+  const network = getNetwork();
+
+  for (let nftArg of args) {
+    const nft = await deploy(ethers, "MockERC721", nftArg);
+    writeDeployment(network, nftArg[0], nft.address, nftArg);
+    console.log(`Mock ${nftArg[0]} deployed to ${network} at ${nft.address}`);
+  }
+});
+
+task("verify:MockNFTs", "Verify all 11 mock NFT collections").setAction(async function (
   taskArguments: TaskArguments,
   { ethers },
 ) {
   const hre = require("hardhat");
-  const network = getNetwork();
-  const addressList = readAddressList();
 
-  const coolCats = await deploy(ethers, "CoolCats", []);
+  for (let nftArg of args) {
+    await hre.run("verifier", {
+      name: nftArg[0],
+    });
+    console.log(`${nftArg[0]} verified`);
+  }
+});
 
-  console.log();
-  console.log(`CoolCats deployed to: ${coolCats.address} on ${network}`);
+task("MockNFT:AddMinter", "Add Test Claim contract as minter to all mock NFTs").setAction(async function (
+  taskArguments: TaskArguments,
+  { ethers },
+) {
+  const addresses = readAddressList()[getNetwork()];
 
-  writeDeployment(network, "CoolCats", coolCats.address, []);
+  if (!addresses["TestClaim"]) {
+    console.log("Test Claim contract not found");
+    return;
+  }
+
+  for (let nftArg of args) {
+    const contract = await ethers.getContractAt("MockERC721", addresses[nftArg[0]]);
+
+    try {
+      await contract.registerMinter(addresses["TestClaim"]);
+      console.log(`Added Test Claim as minter of ${nftArg[0]}`);
+    } catch (e) {
+      console.log("Already added");
+    }
+  }
 });

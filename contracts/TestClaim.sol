c58d31f80d0c639274638c5be66492c696f786e3
LidoKing <55234791+LidoKing@users.noreply.github.com>
2023-01-03 14:09:07 +0800

    feat: changed random minting to mint by order

--- contracts/TestClaim.sol
+++ contracts/TestClaim.sol
@@ -2,26 +2,28 @@
 
 pragma solidity ^0.8.0;
 
+import "./tokens/interfaces/IFurionToken.sol";
+
 /**
  * @title  Claim necessary tokens for testing Furion
  * @notice We will mint sender specific amount of FurionToken, MockUSD, and NFTs
  */
 
 contract TestClaim {
-    address constant BAYC = 0x5A6732E790E812Ce990454A0e81b701dcFc19918;
-    address constant MAYC = 0x62D6275A4BDf7Fe9Af9745D943637Ec96F92c619;
-    address constant OTHERSIDE = 0x91070bC59Be024B6A41Dc163DcCD7F2F351db80e;
-    address constant BAKC = 0xDdc7dE934A83d039929D31f8b6Af8c0614fab92C;
-    address constant PUNKS = 0x90bD8a7d20534a896d057a1F4eA1B574f15F16d5;
-    address constant AZUKI = 0xcf42Cc957bf3A68082d34b3F9DCf96f92C5b14a3;
-    address constant DOODLES = 0x12d804cB8d051C6162B67eE52eB449290e7F4eaf;
-    address constant MEEBITS = 0x0061Da8C3a94FC40D7D12f3180996E7D5C2B586a;
-    address constant GHOST = 0xcB9Eb2Ec8FD2cD94337Dfd6a98eA515B949341A5;
-    address constant CATDDLE = 0x611acdb027e6DC31ead00C63310b24ABDa8a6e38;
-    address constant SHHANS = 0x868B41A59Bb5A80d62b0153fBE53c7be2BEC798A;
+    address constant BAYC = 0x62681C8021a1f1C1C8DDba5992beB192bbBdF9BB;
+    address constant MAYC = 0x10e4A7696B9120A1482958812b655d542b48C37a;
+    address constant OTHERDEED = 0x3Ac6e1030d42AF70D7596EA1C51a563016B24AED;
+    address constant BAKC = 0x9b6E64D58E815D54725eE97B1Ff1901a6F0dad99;
+    address constant PUNKS = 0xa3430ccf668e6BF470eb11892B338ee9f3776AA3;
+    address constant AZUKI = 0x334304e341ac6b4C8246F1F885eEc0C014a5337D;
+    address constant DOODLES = 0x827cA7a1b2b0aaFAA84B74e432C89E8caafB2573;
+    address constant MEEBITS = 0xc37aaA27F8E4674dADD3942e105bC1426bFe69fB;
+    address constant GHOST = 0x4E56446A826FA74A10d03D182cC2047740965Fb9;
+    address constant CATDDLE = 0xAE4753760AC717C2A05c517C201a9aB9ABF0E15C;
+    address constant SHHANS = 0x3064232c2B96936274D9419fA1068dEE7446f249;
     address[] nfts = [
         MAYC,
-        OTHERSIDE,
+        OTHERDEED,
         BAKC,
         PUNKS,
         AZUKI,
@@ -31,17 +33,22 @@ contract TestClaim {
         CATDDLE,
         SHHANS
     ];
-    // Furion has a total supply of 1 billion
+    address constant FUR = 0x167873d27d6f16C503A694814a3895215344B601;
+
     mapping(address => bool) public claimed;
 
-    uint256 private randNum = 192785;
+    uint256 public counter = 0;
 
     /**
      * @notice Claim testing tokens
      */
-    function claimTest() external {
+    function claimTest() external returns (uint256) {
         // every account can only claim once
         require(!claimed[msg.sender], "Already claimed");
+
+        claimed[msg.sender] = true;
+
+        IFurionToken(FUR).mintFurion(msg.sender, 1000e18);
 
         bytes memory data;
         bool success;
@@ -52,19 +59,13 @@ contract TestClaim {
         (success, returnData) = BAYC.call(data);
         require(success, string(returnData));
 
-        uint256 random = _getRandom();
-        (success, returnData) = nfts[random].call(data);
+        uint256 randNft = counter % 10;
+        (success, returnData) = nfts[randNft].call(data);
         require(success, string(returnData));
 
-        claimed[msg.sender] = true;
-    }
+        counter++;
 
-    function _getRandom() private view returns (uint256) {
-        return
-            uint256(
-                keccak256(
-                    abi.encodePacked(block.timestamp, msg.sender, randNum)
-                )
-            ) % 9;
+        // Return nft index for getting the corresponding image at front-end
+        return randNft;
     }
 }
